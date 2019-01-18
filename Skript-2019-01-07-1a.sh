99f307dda26e2eb082c861b25e35b9c69c4eb2e4
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-18 16:06:58 +0100

    Update and rename 2019-01-07-1a.sh to Skript-2019-01-07-1a.sh

---
+++ Skript-2019-01-07-1a.sh
@@ -0,0 +1,16 @@
+
+
+
+
+#!/bin/bash
+# take the username and save it as variable $user
+read -p "Who are you? " user && ((
+
+# check if user is logged in
+who -u | grep -q "^$user "   &&   
+
+# run top with the given username and  if the user is not logged in
+top -u "$user"                   ) || 
+
+#print out an error messege "$user is not logged in"
+echo "$user is not logged in"   )
