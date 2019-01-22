2633a68e3b21a3a38654ccde4e0d8046be9a801c
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-22 15:09:33 +0100

    Create Skript-2019-01-07-1f.sh

---
+++ Skript-2019-01-07-1f.sh
@@ -0,0 +1,19 @@
+
+
+
+#!/bin/bash
+# if gt is smaller or eq with 0 then print error messege otherwise print number of argument an
+if [ $# -eq "0" ] ;then
+    echo "No paramters given"
+else
+    echo -e "\nNumber of arguments: $#\n\n"
+fi
+"set attributes and display values.
+declare -i COUNT=$1
+
+# Count down to 0
+while [ $COUNT -gt 0 ] ;do
+    echo $COUNT
+    let COUNT=$COUNT-1
+done
+echo -e "\nFinished!\n"
