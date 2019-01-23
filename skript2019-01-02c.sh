f1e4f01d16f9f020e50785ac813f4d114ecfb24b
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-23 10:19:50 +0100

    Update skript2019-01-02c.sh

--- skript2019-01-02c.sh
+++ skript2019-01-02c.sh
@@ -1,3 +1,9 @@
+
+
+
 #! /bin/bash
 echo "Change the script so that it retrieves IPv6 addresses instead":
 ip addr | awk '/^[0-9]+/ { currentinterface=$2 } $1 == "inet6\b" { split( $2, foo, "/" ); print currentinterface ,foo[1] }'
+
+
+
