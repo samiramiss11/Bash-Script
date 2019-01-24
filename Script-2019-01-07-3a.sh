7571da65a29ad61a3bf1816a8ffd6ab2eabf8b72
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-24 09:39:02 +0100

    Update Script-2019-01-07-3a.sh

--- Script-2019-01-07-3a.sh
+++ Script-2019-01-07-3a.sh
@@ -1,9 +1,49 @@
 
 
+
+
+#my variable for red color
+b=red
+#write your favorite color.
+echo “type your favariote color: ”
+
+read a
+#check and compare the answer, 
+c=`expr $a = $b`
+
+#If the answer is corrrect print a messege.
+if test $c –eq 1
+then
+echo “your choosen color is : $a”
+
+#If the answer is not correct pring wrong messege.
+else
+while test $c –eq 0
+do
+echo “ Wrong answer”
+
+#sleep for 3sec and clear the screen
+sleep 3
+clear
+
+#  start over and choose your favorite  color
+echo “type your favariote color:”
+
+read a
+c=`expr $a = $b`
+done
+
+echo “your choosen color is : $a”
+
+fi
  
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 #!/bin/bash
 main() {
 echo "write you favariote color" 
@@ -18,12 +58,12 @@ exit
 # if the color is not red print fail messege, wait 5s and clear screen,
 else
 	echo "wrong answer" 
-  sleep 5s &&  clear 
+  sleep 3s &&  clear 
   startover
       fi
 }
 main
- 
+
   
 
 
