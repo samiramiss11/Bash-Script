efeddb28111370c1ab278a6fb7207b6dac1729b1
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-24 09:57:21 +0100

    Update Skript-2019-01-07-1b.sh

--- Skript-2019-01-07-1b.sh
+++ Skript-2019-01-07-1b.sh
@@ -3,6 +3,34 @@
 
 
 #!/bin/bash
+#the file im looking for.
+echo "Please enter your file name here"
+
+#check if the give name exist.if not print file not exist.
+read a
+if [ ! –e $a ]
+then
+echo “file not exist”
+
+#if the file exist check if its executable. 
+elif [ ! –x $a ]
+
+#if so then print This file is executable.
+then
+echo “This file is executable”
+
+#otherwise make it executable and print a messege.
+else
+echo "File has execute permission" 
+
+#give 777 permision to the file
+chmod 777 $a
+
+fi
+
+
+
+#!/bin/bash
 #check if the file is executable
 if [ -x c:/etc/sami.txt ]
 
@@ -13,7 +41,9 @@ then
    else 
      chmod +x c:/etc/sami.txt 
    
-     echo "File has execute permission" 
+     echo "File has execute permission"
+     #give 777 permision to the file
+chmod 777 $a
    fi
 
 
